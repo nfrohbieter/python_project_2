85e9badf313fee2106e1e442de5b058ce72668df
Nathan Frohbieter <nathanrfro83@gmail.com>
2020-05-18 12:38:56 -0500

    worked on the user display

--- application.py
+++ application.py
@@ -1,7 +1,30 @@
-def clean_data():
+#import constants
 
+import random
 
+def display_options1():
+    print("BASKETBALL TEAM STATS TOOL\n")
+    print("\n--- MENU ---\n")
+    print("Here are your choices:")
+    print("1) Display Team Stats")
+    print("2) Quit\n")
 
+def display_options2():
+    print("1) Panthers")
+    print("2) Bandits")
+    print("3) Warriors\n")
 
-def balance_teams():
-    
+display_options1()
+while True:
+    try:
+        user_input1 = int(input("Enter an option > "))
+    except:
+        print("Sorry wrong input. Remember only numbers 1 or 2.")
+    else:
+        if user_input1 == 1:
+            # do later
+            continue
+        elif user_input1 == 2:
+            break
+        else:
+            print("Sorry wrong input. Remember only numbers 1 or 2.")
