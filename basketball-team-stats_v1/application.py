fca5a6898da2c7051f3d7540a0fd9e509c27b0a5
Nathan Frohbieter <nathanrfro83@gmail.com>
2020-05-21 14:22:06 -0500

    created team output function

--- basketball-team-stats_v1/application.py
+++ basketball-team-stats_v1/application.py
@@ -2,47 +2,80 @@ from constants import PLAYERS
 
 import random
 
-def balance_teams():
-    team1 = []
-    team2 = []
-    team3 = []
+Panthers = []
+Bandits = []
+Warriors = []
+
+
+def clean_data():
     for player in PLAYERS:
-        random_number = random.randint(1,3)
-        if random_number == 1 and len(team1) <6:
-            team1.append(player)
-        elif random_number == 2 and len(team2) <6:
-            team2.append(player)
-        elif random_number == 3 <6:
-            team3.append(player)
+        height = player.get('height')
+        slice_string = slice(2)
+        height = int(height[slice_string])
+        player['height'] = height
+        if player.get('experience') == 'YES':
+            player['experience'] = True
         else:
-            if len(team1) <6:
-                team1.append(player)
-            elif len(team2) <6:
-                team2.append(player)
-            elif len(team3) <6:
-                team3.append(player)
+            player['experience'] = False
+    return PLAYERS
 
-    print(f"team 1 {team1}")
-    print(f"team 2 {team2}")
-    print(f"team 3 {team3}")
 
-def display_options1():
+def balance_teams():
+    for player in PLAYERS:
+        random_number = random.randint(1, 3)
+        if random_number == 1 and len(Panthers) < 6:
+            Panthers.append(player)
+        elif random_number == 2 and len(Bandits) < 6:
+            Bandits.append(player)
+        elif random_number == 3 and len(Warriors) < 6:
+            Warriors.append(player)
+        else:
+            if len(Panthers) < 6:
+                Panthers.append(player)
+            elif len(Bandits) < 6:
+                Bandits.append(player)
+            elif len(Warriors) < 6:
+                Warriors.append(player)
+    return Panthers, Bandits, Warriors
+
+
+def display_options():
     print("BASKETBALL TEAM STATS TOOL\n")
     print("\n--- MENU ---\n")
     print("Here are your choices:")
     print("1) Display Team Stats")
     print("2) Quit\n")
 
-def display_options2():
+
+def display_team_options():
     print("1) Panthers")
     print("2) Bandits")
     print("3) Warriors\n")
 
-if __name__ == '__main__':
-    balance_teams()
 
-#display_options1()
-#while True:
+def display_team(team_number):
+    if team_number == 1:
+        print("Panthers Roster")
+        print(Panthers)
+    elif team_number == 2:
+        print("Bandits Roster")
+        print(Bandits)
+    elif team_number == 3:
+        print("Warriors Roster")
+        print(Warriors)
+    else:
+        print("Invalid input. Please pick a number 1-3.")
+
+
+if __name__ == '__main__':
+    clean_data()
+    balance_teams()
+    display_team_options()
+    team_input = int(input("Enter an option > "))
+    display_team(team_input)
+
+# display_options1()
+# while True:
 #    try:
 #        user_input1 = int(input("Enter an option > "))
 #    except:
